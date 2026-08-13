492de4521156e9260f5d8700b13438cdd9540d62
moon18-eng <moo203417@gmail.com>
2026-08-14 00:09:36 +0100

    second

--- frontend/src/Components/formPage.jsx
+++ frontend/src/Components/formPage.jsx
@@ -19,19 +19,25 @@ function FormPage() {
 
   return (
     <div className='flex-1 flex flex-col  '>
-      <button className='flex items-start ml-12 my-5 w-fit border-2 border-black bg-gray-400 rounded-md py-2 px-6 font-extrabold text-xl'>
-       Cancel
+      <button className='flex items-start ml-20 mt-14 w-fit border-4 border-black bg-gray-400 rounded-md py-1 px-8 font-extrabold text-2xl '>
+       ◄  Cancel
       </button>
 
-      <form className='flex flex-row flex-1 gap-8' onSubmit={handelForm}>
-        <div className=' flex flex-col w-2/3 mb-12 ml-12 border-2 border-black bg-gray-200 rounded-md py-2 px-6 '>
-          <input className='h-20 border-2 border-gray-700 bg-transparent focus:outline-none border-none placeholder:text-gray-700 text-5xl font-extrabold px-5 pt-5 mb-8' placeholder='Title...' type='text' value={formData.title} onChange={(e) => setFormData({...formData,title:e.target.value})}/>
-          <textarea className='flex-1 border-2 border-gray-700 bg-transparent focus:outline-none border-none placeholder:text-gray-500 text-2xl placeholder:zfont-extrabold px-5 pb-5' placeholder='Enter a text...' type='text' value={formData.content} onChange={(e) => setFormData({...formData,content:e.target.value})}/>
+      <form className='flex flex-row flex-1 gap-10 m-10' onSubmit={handelForm}>
+        <div className='flex flex-col w-3/4 mb-12 ml-12 border-4 border-gray-600 bg-gray-200 rounded-md py-2 px-6 '>
+          <input className='h-20 border-2 border-gray-700 bg-transparent focus:outline-none border-none placeholder:text-gray-700 text-7xl font-extrabold px-5 pt-5 mb-10' placeholder='Title...' type='text' value={formData.title} onChange={(e) => setFormData({...formData,title:e.target.value})}/>
+          <textarea className='flex-1 border-2 border-gray-700 bg-transparent focus:outline-none border-none placeholder:text-gray-500 text-3xl placeholder:zfont-extrabold px-5 pb-5' placeholder='Enter a text...' type='text' value={formData.content} onChange={(e) => setFormData({...formData,content:e.target.value})}/>
         </div>
-        <div className=' flex flex-col gap-8 items-start justify-center w-1/3 mb-12 mr-12 border-2 border-black bg-gray-400 rounded-md py-2 px-6 '>
+        <div className=' flex flex-col gap-8 items-center justify-center w-1/4 mb-12 mr-12 border-4 border-gray-600 bg-gray-400 rounded-md py-2 px-6 '>
+         <div className='flex flex-row items-center gap-24 border-4 border-white bg-gray-300 rounded-xl text-3xl font-extrabold text-gray-600 px-8 py-3'>
+          <p>Bg color</p>
           <input className='h-16 w-20 p-0 [&::-webkit-color-swatch-wrapper]:p-0 [&::-webkit-color-swatch]:border-none [&::-moz-color-swatch]:border-none border-2 border-white' type='color' value={formData.bg_color} onChange={(e) => setFormData({...formData,bg_color:e.target.value})}/>
+         </div>
+          <div className='flex flex-row items-center gap-16 border-4 border-white bg-gray-300 rounded-xl text-3xl font-extrabold text-gray-600 px-8 py-3'>
+          <p>Text color</p>
           <input className='h-16 w-20  p-0 [&::-webkit-color-swatch-wrapper]:p-0 [&::-webkit-color-swatch]:border-none [&::-moz-color-swatch]:border-none border-2 border-white' type='color' value={formData.text_color} onChange={(e) => setFormData({...formData,text_color:e.target.value})}/>
-          <button type='submit' className='border-4 border-yellow-600 bg-yellow-200 text-yellow-600 font-extrabold'>Confirm</button>
+         </div>
+          <button type='submit' className='border-4 border-yellow-600 bg-yellow-200 text-yellow-600 font-extrabold text-2xl rounded-xl px-32 py-3 mt-12'>Confirm</button>
         </div>
       </form>
     
